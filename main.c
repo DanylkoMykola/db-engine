3fa4eef50c5a416e21c5ec7c17a581cc04ed4901
mdanylk <mdanylko@agileassets.com>
2025-08-31 02:07:31 +0300

    handle file system

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
+
+#define USERNAME_SIZE 32
+#define EMAIL_SIZE 255
+#define FILENAME "my.db"
+
+// ---------------------------
+// Row structure
+// ---------------------------
+typedef struct {
+    uint32_t id;
+    char username[USERNAME_SIZE];
+    char email[EMAIL_SIZE];
+} Row;
 //
 // Input buffer
 //
@@ -75,11 +89,24 @@ typedef enum {
 
 typedef struct {
     StetmentType type;
+    Row rowToInsert; // used only for INSERT
 } Statement;
 
 PrepareResult prepareStatement(InputBuffer* inputBuffer, Statement* statement) {
     if (strncmp(inputBuffer -> buffer, "insert", 6) == 0) {
         statement -> type = STATEMENT_INSERT;
+         // simple parsing: "insert id username email"
+        int id = 0;
+        char username[USERNAME_SIZE];
+        char email[EMAIL_SIZE];
+        int n.exit = sscanf(inputBuffer->buffer, "insert %d %31s %254s", &id, username, email);
+        if (n < 3) {
+            printf("Syntax error. Use: insert <id> <username> <email>\n");
+            return PREPARE_UNRECOGNIZED_STATEMENT; // failure
+        }
+        statement->rowToInsert.id = (uint32_t)id;
+        strncpy(statement->rowToInsert.username, username, USERNAME_SIZE);
+        strncpy(statement->rowToInsert.email, email, EMAIL_SIZE);
         return PREPARE_SUCCESS;
     }
     if (strcmp(inputBuffer -> buffer, "select") == 0) {
@@ -92,12 +119,30 @@ PrepareResult prepareStatement(InputBuffer* inputBuffer, Statement* statement) {
 
 void executeStatement(Statement* statement) {
     switch (statement->type) {
-        case STATEMENT_INSERT:
-            printf("This is where we would do an insert.\n");
+        case STATEMENT_INSERT: {
+             FILE* fp = fopen(FILENAME, "ab"); // append binary
+            if (!fp) {
+                printf("Could not open file for writing.\n");
+                return;
+            }
+            fwrite(&(statement->rowToInsert), sizeof(Row), 1, fp);
+            fclose(fp);
+            printf("Inserted.\n");
             break;
-        case STATEMENT_SELECT:
-            printf("This is where we would do a select.\n");
+        }
+        case STATEMENT_SELECT: {
+            FILE* fp = fopen(FILENAME, "rb");
+            if (!fp) {
+                printf("Could not open file for reading.\n");
+                return;
+            }
+            Row row;
+            while (fread(&row, sizeof(Row), 1, fp) == 1) {
+                printf("%d %s %s\n", row.id, row.username, row.email);
+            }
+            fclose(fp);
             break;
+        }
     }
 }
 
